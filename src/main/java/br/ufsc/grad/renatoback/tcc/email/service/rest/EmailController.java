5d354826de4da3eca3cd9838f88c1ec7e29958e9
Renato Pereira Back <renato.back@micromed.com.br>
2016-11-16 13:58:16 -0200

    Adicionado método para imprimir hora no servidor

--- src/main/java/br/ufsc/grad/renatoback/tcc/email/service/rest/EmailController.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/email/service/rest/EmailController.java
@@ -32,4 +32,10 @@ public class EmailController {
 		service.printStatistics(threads, sleep);
 	}
 
+	@RequestMapping(method = RequestMethod.OPTIONS)
+	@ResponseStatus(code = HttpStatus.OK)
+	public void printTime() {
+		System.out.println("Nano time: " + System.nanoTime());
+	}
+
 }
